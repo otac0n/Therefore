84eb8bd2a0bf2507b931f95c921730d41ed71c48
John Gietzen <otac0n@gietzen.us>
2011-07-15 22:11:02 -0500

    Added preliminary parts of the parser.

---
+++ Therefore.Engine/Parser/Span.cs
@@ -0,0 +1,50 @@
+﻿namespace Therefore.Engine.Parser
+{
+    using System;
+
+    public sealed class Span
+    {
+        private readonly string source;
+        private readonly int start;
+        private readonly int length;
+
+        public Span(string source, int start, int length)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            this.source = source;
+
+            if (start < 0 || start > source.Length)
+            {
+                throw new ArgumentOutOfRangeException("start");
+            }
+
+            this.start = start;
+
+            if (length < 0 || (start + length) > source.Length)
+            {
+                throw new ArgumentOutOfRangeException("length");
+            }
+
+            this.length = length;
+        }
+
+        public string Source
+        {
+            get { return this.source; }
+        }
+
+        public int Start
+        {
+            get { return this.start; }
+        }
+
+        public int Length
+        {
+            get { return this.length; }
+        }
+    }
+}
---
+++ Therefore.Engine/Parser/Token.cs
@@ -0,0 +1,42 @@
+﻿namespace Therefore.Engine.Parser
+{
+    using System;
+
+    public enum TokenType
+    {
+        Variable,
+        BinaryOperator,
+        UnaryOperator,
+        LeftParenthesis,
+        RightParenthesis,
+        EOF,
+    }
+
+    public sealed class Token
+    {
+        private readonly TokenType tokenType;
+        private readonly Span span;
+
+        public Token(TokenType tokenType, Span span)
+        {
+            this.tokenType = tokenType;
+
+            if (span == null)
+            {
+                throw new ArgumentNullException("span");
+            }
+
+            this.span = span;
+        }
+
+        public TokenType TokenType
+        {
+            get { return this.tokenType; }
+        }
+
+        public Span Span
+        {
+            get { return this.span; }
+        }
+    }
+}
