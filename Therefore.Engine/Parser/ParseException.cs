7c3a2869a502ef2b71c79e12ee08918009c7a5c1
John Gietzen <otac0n@gietzen.us>
2011-07-15 22:41:16 -0500

    Implemented Scanner/Lexer.

---
+++ Therefore.Engine/Parser/ParseException.cs
@@ -0,0 +1,85 @@
+﻿namespace Therefore.Engine.Parser
+{
+    using System;
+    using System.Linq;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Thrown when there is an error parsing.
+    /// </summary>
+    [Serializable]
+    public class ParseException : Exception
+    {
+        private readonly int offset;
+
+        /// <summary>
+        /// Initializes a new instance of the ParseException class.
+        /// </summary>
+        public ParseException(int offset, params TokenType[] tokenTypes)
+            : this(FormatMessage(tokenTypes, offset), offset)
+        {
+        }
+
+        private static string FormatMessage(TokenType[] tokenTypes, int offset)
+        {
+            if (tokenTypes.Length == 0)
+            {
+                return string.Format("Expected (unkonwn) at character {0}.", offset);
+            }
+
+            if (tokenTypes.Length == 1)
+            {
+                return string.Format("Expected {0} at character {1}.", tokenTypes[0], offset);
+            }
+
+            var tokenTypeList = string.Join(", ", tokenTypes.Take(tokenTypes.Length - 1).Select(t => t.ToString())) + " or " + tokenTypes[tokenTypes.Length - 1];
+            return string.Format("Expected {0} at character {1}.", tokenTypeList, offset);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ParseException class.
+        /// </summary>
+        public ParseException(string message, int offset)
+            : this(message)
+        {
+            this.offset = offset;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ParseException class with a specified error message.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        public ParseException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ParseException class with a specified error
+        /// message and a reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="message">The message that describes the error.</param>
+        /// <param name="inner">The exception that is the cause of the current exception, or a null reference (Nothing in Visual Basic) if no inner exception is specified.</param>
+        public ParseException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ParseException class with serialized data.
+        /// </summary>
+        /// <param name="info">The System.Runtime.Serialization.SerializationInfo that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The System.Runtime.Serialization.StreamingContext that contains contextual information about the source or destination.</param>
+        /// <exception cref="System.ArgumentNullException">The info parameter is null.</exception>
+        /// <exception cref="System.Runtime.Serialization.SerializationException">The class name is null or System.Exception.HResult is zero (0).</exception>
+        protected ParseException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        public int Offset
+        {
+            get { return this.offset; }
+        }
+    }
+}
---
+++ Therefore.Engine/Parser/Scanner.cs
@@ -0,0 +1,188 @@
+﻿namespace Therefore.Engine.Parser
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using Therefore.Engine.Expressions;
+
+    public static class Scanner
+    {
+        private readonly static Dictionary<TokenType, Func<string, int, int?>> terminals = new Dictionary<TokenType, Func<string, int, int?>>
+        {
+            { TokenType.Variable, MakeRegex(@"\w+") },
+            { TokenType.LeftParenthesis, MakeLiteral('(') },
+            { TokenType.RightParenthesis, MakeLiteral(')') },
+            { TokenType.UnaryOperator, MakeLiteral('~') },
+            { TokenType.BinaryOperator, MakeLiteral('&', '|', '>') },
+            { TokenType.EOF, (source, location) =>
+                {
+                    return location == source.Length ? (int?)0 : null;
+                }
+            },
+        };
+
+        private static Func<string, int, int?> MakeLiteral(params char[] literals)
+        {
+            return (source, offset) =>
+            {
+                foreach (var literal in literals)
+                {
+                    if (offset < source.Length && source[offset] == literal)
+                    {
+                        return 1;
+                    }
+                }
+
+                return null;
+            };
+        }
+
+        private static Func<string, int, int?> MakeRegex(string pattern)
+        {
+            var regex = new Regex(pattern, RegexOptions.Compiled);
+
+            return (source, offset) =>
+            {
+                var match = regex.Match(source, offset);
+                if (match.Success && match.Index == offset)
+                {
+                    return match.Length;
+                }
+
+                return null;
+            };
+        }
+
+        private static int Advance(Span span)
+        {
+            var source = span.Source;
+            var offset = span.Start + span.Length;
+
+            while (offset < span.Length && char.IsWhiteSpace(source, offset))
+            {
+                offset++;
+            }
+
+            return offset;
+        }
+
+        private static Token Accept(TokenType tokenType, Cursor cursor)
+        {
+            var terminal = terminals[tokenType];
+            var length = terminal(cursor.Source, cursor.Offset);
+            if (length == null)
+            {
+                return null;
+            }
+
+            var span = new Span(cursor.Source, cursor.Offset, length.Value);
+            cursor.Offset = Advance(span);
+
+            return new Token(tokenType, span);
+        }
+
+        private static Token Expect(TokenType tokenType, Cursor cursor)
+        {
+            var token = Accept(tokenType, cursor);
+            if (token == null)
+            {
+                throw new ParseException(cursor.Offset, tokenType);
+            }
+
+            return token;
+        }
+
+        public static IEnumerable<Token> Scan(string source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+
+            var cursor = new Cursor(source);
+
+            foreach (var token in ScanExpression(cursor))
+            {
+                yield return token;
+            }
+
+            yield return Expect(TokenType.EOF, cursor);
+        }
+
+        private static IEnumerable<Token> ScanExpression(Cursor cursor)
+        {
+            foreach (var token in ScanAtomicExpression(cursor))
+            {
+                yield return token;
+            }
+
+            Token binaryOp;
+            while ((binaryOp = Accept(TokenType.BinaryOperator, cursor)) != null)
+            {
+                yield return binaryOp;
+
+                foreach (var token in ScanAtomicExpression(cursor))
+                {
+                    yield return token;
+                }
+            }
+        }
+
+        private static IEnumerable<Token> ScanAtomicExpression(Cursor cursor)
+        {
+            var variable = Accept(TokenType.Variable, cursor);
+            if (variable != null)
+            {
+                yield return variable;
+                yield break;
+            }
+
+            var unaryOp = Accept(TokenType.UnaryOperator, cursor);
+            if (unaryOp != null)
+            {
+                yield return unaryOp;
+                foreach (var token in ScanAtomicExpression(cursor))
+                {
+                    yield return token;
+                }
+                yield break;
+            }
+
+            var leftParen = Accept(TokenType.LeftParenthesis, cursor);
+            if (leftParen != null)
+            {
+                yield return leftParen;
+                foreach (var token in ScanExpression(cursor))
+                {
+                    yield return token;
+                }
+                yield return Expect(TokenType.RightParenthesis, cursor);
+                yield break;
+            }
+
+            throw new ParseException(cursor.Offset, TokenType.UnaryOperator, TokenType.Variable, TokenType.LeftParenthesis);
+        }
+
+        private class Cursor
+        {
+            private readonly string source;
+
+            public Cursor(string source)
+            {
+                if (source == null)
+                {
+                    throw new ArgumentNullException("source");
+                }
+
+                this.source = source;
+            }
+
+            public string Source
+            {
+                get { return this.source; }
+            }
+
+            public int Offset { get; set; }
+        }
+    }
+}
