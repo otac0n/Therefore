a1d0cec3575e3ec5a94cd5749b7d8651f94ac3d8
John Gietzen <otac0n@gietzen.us>
2011-07-15 19:53:31 -0500

    Added an example program with helper functions for expressing logic sentences.

---
+++ Therefore/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Therefore
+{
+    using System;
+    using Therefore.Engine.Expressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expression = And(And(A, Not(B)), Or(C, Not(C)));
+        }
+
+        public static AndExpression And(Expression left, Expression right)
+        {
+            return new AndExpression(left, right);
+        }
+
+        public static OrExpression Or(Expression left, Expression right)
+        {
+            return new OrExpression(left, right);
+        }
+
+        public static NotExpression Not(Expression operand)
+        {
+            return new NotExpression(operand);
+        }
+
+        public static VariableExpression A
+        {
+            get
+            {
+                return new VariableExpression(0);
+            }
+        }
+
+        public static VariableExpression B
+        {
+            get
+            {
+                return new VariableExpression(1);
+            }
+        }
+
+        public static VariableExpression C
+        {
+            get
+            {
+                return new VariableExpression(2);
+            }
+        }
+
+        public static VariableExpression D
+        {
+            get
+            {
+                return new VariableExpression(3);
+            }
+        }
+    }
+}
