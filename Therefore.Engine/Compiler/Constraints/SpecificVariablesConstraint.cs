3506b633a1e74f3a54f7f9f21acc81b00efee4a8
John Gietzen <otac0n@gietzen.us>
2011-07-18 19:44:20 -0500

    Added the `SpecificVariablesConstraint` constraint, for limiting the available variables.

---
+++ Therefore.Engine/Compiler/Constraints/SpecificVariablesConstraint.cs
@@ -0,0 +1,52 @@
+﻿namespace Therefore.Engine.Compiler.Constraints
+{
+    using System;
+    using System.Linq;
+    using Therefore.Engine.Parser.Nodes;
+    using System.Collections.Generic;
+
+    public sealed class SpecificVariablesConstraint : Constraint
+    {
+        private readonly string[] variables;
+        private readonly IEqualityComparer<string> comparison;
+
+        public SpecificVariablesConstraint(string[] variables, IEqualityComparer<string> comparison = null)
+        {
+            if (variables == null)
+            {
+                throw new ArgumentNullException("variables");
+            }
+
+            this.variables = variables.ToArray();
+
+            this.comparison = comparison ?? EqualityComparer<string>.Default;
+        }
+
+        public override ConstraintViolation VisitBinaryOperatorNode(BinaryOperatorNode binNode)
+        {
+            return null;
+        }
+
+        public override ConstraintViolation VisitUnaryOperatorNode(UnaryOperatorNode unNode)
+        {
+            return null;
+        }
+
+        public override ConstraintViolation VisitParenthesisNode(ParenthesisNode parenNode)
+        {
+            return null;
+        }
+
+        public override ConstraintViolation VisitVariableNode(VariableNode varNode)
+        {
+            var value = varNode.Variable.Value;
+
+            if (!this.variables.Any(v => this.comparison.Equals(value, v)))
+            {
+                return new ConstraintViolation(varNode, "The variable name '" + value + "' is not allowed.");
+            }
+
+            return null;
+        }
+    }
+}
