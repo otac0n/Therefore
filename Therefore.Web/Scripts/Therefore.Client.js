cc8440fe7fbf0dc0344bedf826c8c88f48fc9f64
John Gietzen <otac0n@gietzen.us>
2011-07-19 17:00:27 -0500

    Added a client-side library.

---
+++ Therefore.Web/Scripts/Therefore.Client.js
@@ -0,0 +1,7 @@
+﻿/// <reference path="jquery-1.4.4-vsdoc.js" />
+
+var Therefore = { rootUrl = "/" };
+
+Therefore.parse = function (statement, callback) {
+    $.get(this.rootUrl + "Game/Parse", { statement: statement }, callback, "json");
+}
