09b92ef11eeb029f62beae9c87e359d637e3c46d
Katie Gietzen <goddessartemis85@gmail.com>
2011-07-29 22:20:36 -0500

    Added customizations to the cards and the random wood grain background.

---
+++ Therefore.Web/HtmlHelpers.cs
@@ -0,0 +1,18 @@
+﻿namespace Therefore.Web
+{
+    using System;
+    using System.Web.Mvc;
+
+    public static class HtmlHelpers
+    {
+        private readonly static Random random = new Random();
+
+        public static int Random(this HtmlHelper html, int maximum)
+        {
+            lock (random)
+            {
+                return random.Next(maximum);
+            }
+        }
+    }
+}
